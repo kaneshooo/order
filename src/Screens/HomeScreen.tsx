10e0f00b4d34ef52d88857f4f06d869291f33d02
kousaku1122 <kou1122saku@gmail.com>
2021-08-19 13:26:42 +0900

    Create HomeScreen

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,60 +1,63 @@
 import React from "react";
-import { StyleSheet, Button, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.wrapper}>
-        <Text style={styles.title}>HOME</Text>
-        <Button
-          style={styles.startButton}
-          title="start"
-          onPress={() => this.props.navigation.navigate("Order")}
-        />
-        <Button
-          style={styles.logButton}
-          title="Log"
-          onPress={() => this.props.navigation.navigate("Log")}
-        />
-      </View>
-    );
-  }
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>HOME</Text>
+
+      <TouchableOpacity
+        style={styles.startButton}
+        onPress={() => navigation.navigate("Order")}
+      >
+        <Text style={styles.buttonSize}>start</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logButton}
+        onPress={() => navigation.navigate("Log")}
+      >
+        <Text style={styles.buttonSize}>Log</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    display: "flex" /* 子要素をflexboxで揃える */,
-    flexDirection: "column" /* 子要素をflexboxにより縦方向に揃える */,
-    justifyContent: "center" /* 子要素をflexboxにより中央に配置する */,
-    alignItems: "center" /* 子要素をflexboxにより中央に配置する */
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
   title: {
     borderRadius: "solid",
     fontSize: 40,
     marginTop: 50,
-    width: 200,
-    height: 90,
+    width: 300,
+    height: 300,
     borderRadius: 20,
     padding: 15,
     textAlign: "center"
   },
   startButton: {
-    marginTop: 180,
     fontSize: 30,
     width: 150,
     height: 70,
     backgroundColor: "#99ff66",
-    borderRadius: 20
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonSize: {
+    fontSize: 30
   },
   logButton: {
-    marginTop: 20,
-    fontSize: 30,
     width: 150,
     height: 70,
     backgroundColor: "#0099ff",
-    borderRadius: 20
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10
   }
 });
 
