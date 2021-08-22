3d24b7af5edd9ce266e565ee67810d912e29e515
kousaku1122 <kou1122saku@gmail.com>
2021-08-23 03:25:27 +0900

    Create ImgList

--- src/components/ImgList.tsx
+++ src/components/ImgList.tsx
@@ -1,64 +1,69 @@
 import React from "react";
-import styled from "styled-components";
-
-
-import ann from "./img/ann.jpg";
-import ball from "./img/ball.jpeg";
-import koin from "./img/koin.jpeg";
-import syateki from "./img/syateki.jpg";
-import takoyaki from "./img/takoyaki.jpg";
-import watagashi from "./img/watagashi.png";
-import yakisoba from "./img/yakisoba.jpg";
-import yoyo from "./img/yoyo.jpg";
-
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Text,
+  Button,
+  Image
+} from "react-native";
+import ann from "../img/ann.jpg";
+import ball from "../img/ball.jpeg";
+import koin from "../img/koin.jpeg";
+import syateki from "../img/syateki.jpg";
+import takoyaki from "../img/takoyaki.jpg";
+import watagashi from "../img/watagashi.png";
+import yakisoba from "../img/yakisoba.jpg";
+import yoyo from "../img/yoyo.jpg";
 
 const itemData = [
   {
     img: ann,
     product_name: "お面",
-    price: 300,
+    price: 300
   },
   {
     img: ball,
     product_name: "ぼーるすくい",
-    price: 200,
+    price: 200
   },
   {
     img: koin,
     product_name: "コイン落とし",
-    price: 200,
+    price: 200
   },
   {
     img: syateki,
     product_name: "射的",
-    price: 200,
+    price: 200
   },
   {
     img: takoyaki,
     product_name: "たこ焼き",
-    price: 500,
+    price: 500
   },
   {
     img: watagashi,
     product_name: "わたがし",
-    price: 400,
+    price: 400
   },
   {
     img: yakisoba,
     product_name: "やきそば",
-    price: 500,
+    price: 500
   },
   {
     img: yoyo,
     product_name: "ヨーヨーすくい",
-    price: 200,
-  },
+    price: 200
+  }
 ];
 
-const ImgList = (props) => {
+const ImgList = props => {
   const { totalAmount, setTotalAmount, item_list, setItemList } = props;
 
-  const checkArray = (product_name) => {
+  const checkArray = product_name => {
     let exist;
     item_list.find((item, index) => {
       if (item.productName === product_name) {
@@ -80,33 +85,38 @@ const ImgList = (props) => {
     setTotalAmount(totalAmount + price);
   };
   return (
-    // <Wrapper>
-    //   <ImageList2 rowHeight={180}>
-    //     {itemData.map((item) => (
-    //       <ImageListItem
-    //         key={item.img}
-    //         onClick={() => setOrderInfo(item.product_name, item.price)}
-    //       >
-    //         <img src={item.img} alt={item.product_name} />
-    //         <ImageListItemBar
-    //           title={item.product_name}
-    //           subtitle={<span>{item.price}円</span>}
-    //         />
-    //       </ImageListItem>
-    //     ))}
-    //   </ImageList2>
-    // </Wrapper>
+    <View>
+      <FlatList
+        style={styles.orderMenu}
+        data={itemData}
+        numColumns={2}
+        keyExtractor={item => item.product_name}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => setOrderInfo(item.product_name, item.price)}
+          >
+            <Image style={styles.img} source={item.img} />
+            <Text>
+              {item.product_name} {item.price}円
+            </Text>
+          </TouchableOpacity>
+        )}
+      ></FlatList>
+    </View>
   );
 };
-const ImageList2 = styled.div`
-  width: 300px;
-  height: 480px;
-`;
-const Wrapper = styled.div`
-  display: "flex";
-  flex-wrap: "wrap";
-  justify-content: "space-around";
-  overflow: "hidden";
-`;
-
+const styles = StyleSheet.create({
+  img: {
+    width: "100%",
+    height: 100
+  },
+  item: {
+    width: "50%",
+    height: 120,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5
+  }
+});
 export default ImgList;
