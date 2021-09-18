67779f32debe6d0c4088e8c85aa47553ad992e92
kousaku1122 <kou1122saku@gmail.com>
2021-09-18 21:31:28 +0900

    remove .

--- src/components/ImgList.tsx
+++ src/components/ImgList.tsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   TouchableOpacity,
@@ -8,68 +8,100 @@ import {
   Button,
   Image
 } from "react-native";
-import { firebase } from '@firebase/app';
-import 'firebase/storage'; 
-import 'firebase/firestore';
+import { firebase } from "@firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
 const ImgList = props => {
-  const { totalAmount, setTotalAmount, item_list, setItemList,orderNumber } = props;
-  var db=firebase.firestore();
-  var date=new Date();
-  var today=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()
-  const [data,setdata]=useState();
-  
-  const dbget=async()=>{
-    const docRef= db.collection("users").doc("menu").collection("ゲーム");
-    const earnings=db.collection("users").doc('earning').collection("list").doc(String(today));
-    const result=await docRef.get().then(
-      querySnapshot => {
-        let bb=[]; 
-        querySnapshot.forEach
-          (doc=> {
-            bb.push(
-              Object.assign({
-              id:doc.id,
-              // page:pagename,
-              url:doc.data().url,
-              name:doc.data().name,
-              price:doc.data().price
-              })
-            )           
-            earnings.set({
-              [doc.data().name]:{
-                  price:doc.data().price,
-                  num:0
-              }
-            },{merge:true})           
+  const {
+    totalAmount,
+    setTotalAmount,
+    item_list,
+    setItemList,
+    orderNumber
+  } = props;
+  var db = firebase.firestore();
+  var date = new Date();
+  var today =
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  const [data, setdata] = useState();
+
+  const dbget = async () => {
+    const docRef = db
+      .collection("users")
+      .doc("menu")
+      .collection("ゲーム");
+    const earnings = db
+      .collection("users")
+      .doc("earning")
+      .collection("list")
+      .doc(String(today));
+    const result = await docRef.get().then(querySnapshot => {
+      let bb = [];
+      querySnapshot.forEach(doc => {
+        bb.push(
+          Object.assign({
+            id: doc.id,
+            // page:pagename,
+            url: doc.data().url,
+            name: doc.data().name,
+            price: doc.data().price
           })
-        return bb;
-      });   
+        );
+        earnings.set(
+          {
+            [doc.data().name]: {
+              price: doc.data().price,
+              num: 0
+            }
+          },
+          { merge: true }
+        );
+      });
+      return bb;
+    });
     setdata(result);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     dbget();
-  },[])
+  }, []);
 
-  const checkArray = (product_name,time) => {
+  const checkArray = (product_name, time) => {
     let exist;
-    for (var i in item_list){
-      if(item_list[i].name==product_name){
-        item_list[i].num+=1;
-        exist=true;
+    for (var i in item_list) {
+      if (item_list[i].name == product_name) {
+        item_list[i].num += 1;
+        exist = true;
       }
     }
     return exist;
-  }
-  const setOrderInfo = (item) => {
-   var time=date.getMonth()+1+"-"+date.getDate()+"-"+date.getHours()+"-"+date.getMinutes()+"-"+date.getSeconds()+"-"+date.getMilliseconds();
-   if (!checkArray(item.name,time)){
-      item_list.[time]=Object.assign({name: item.name, num: 1, price:item.price, time:time ,check:false})
-      };
+  };
+  const setOrderInfo = item => {
+    var time =
+      date.getMonth() +
+      1 +
+      "-" +
+      date.getDate() +
+      "-" +
+      date.getHours() +
+      "-" +
+      date.getMinutes() +
+      "-" +
+      date.getSeconds() +
+      "-" +
+      date.getMilliseconds();
+    if (!checkArray(item.name, time)) {
+      item_list[time] = Object.assign({
+        name: item.name,
+        num: 1,
+        price: item.price,
+        time: time,
+        check: false
+      });
+    }
     setTotalAmount(totalAmount + item.price);
   };
-
 
   return (
     <View>
@@ -81,9 +113,9 @@ const ImgList = props => {
         renderItem={({ item }) => (
           <TouchableOpacity
             style={styles.item}
-            onPress={() => setOrderInfo(item) }
-            >
-            <Image style={styles.img} source={{uri:item.url}}  />
+            onPress={() => setOrderInfo(item)}
+          >
+            <Image style={styles.img} source={{ uri: item.url }} />
             <Text>{item.name}</Text>
             <Text>{item.price}円</Text>
           </TouchableOpacity>
@@ -92,22 +124,22 @@ const ImgList = props => {
     </View>
   );
 };
-  const styles = StyleSheet.create({
-    img: {
-      width: "90%",
-      height: 100,
-      borderRadius: 10,
-      marginBottom: 5,
-      marginTop: 5
-    },
-    item: {
-      width: "48%",
-      height: 140,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#FFF",
-      margin: 2,
-      borderRadius: 10
-    }
-  });
+const styles = StyleSheet.create({
+  img: {
+    width: "90%",
+    height: 100,
+    borderRadius: 10,
+    marginBottom: 5,
+    marginTop: 5
+  },
+  item: {
+    width: "48%",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFF",
+    margin: 2,
+    borderRadius: 10
+  }
+});
 export default ImgList;
